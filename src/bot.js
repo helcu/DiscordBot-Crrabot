72e60afab2a3aac17c89befa454cd67e7ca4ed01
helmutcutipa <helcu@hotmail.com>
2021-02-26 09:43:36 -0500

    Connection with Cryptocyrrency API

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 //console.log(process.env.DISCORDJS_BOT_TOKEN);
+let price = require('crypto-price')
 
 const {Client, MessageAttachment,MessageEmbed} = require('discord.js');
 const client = new Client();
@@ -21,11 +22,44 @@ if(messageCommand.startsWith(PREFIX))
     const [CMD_NAME, ...args] =  messageCommand.trim().substring(PREFIX.length).split(/\s+/);
 
     switch (CMD_NAME) {
-        case 'dogeprice':
-            message.channel.send('Tu da mun!');
+        case 'precio':
+
+            
+            price.getCryptoPrice('USD', args[0].toUpperCase()).then(obj => { // Base for ex - USD, Crypto for ex - ETH 
+                console.log(obj)
+                //message.channel.send(JSON.stringify(obj.price));
+
+
+                const embed = new MessageEmbed()
+                // Set the title of the field
+                .setTitle('CryptoCrrano el precio del ' +args[0].toUpperCase() + ' es:' )
+                // Set the color of the embed
+                .setColor(0xff0000)
+                // Set the main content of the embed
+                .setDescription('Precio: ' + obj.price + ' USD' + '\n' + 'Volumen: ' + obj.volume + '\n' + 'Cambio: ' + obj.change );
+                
+                message.channel.send(embed);
+
+            }).catch(err => {
+                const embed = new MessageEmbed()
+                // Set the title of the field
+                .setTitle('Crrano Gil!' )
+                // Set the color of the embed
+                .setColor(0xff0000)
+                // Set the main content of the embed
+                .setDescription('La crypto ' + args[0] + 'no existe webonaso');
+                
+                message.channel.send(embed);
+                console.log(err)
+            })
+
+            
           break;
         case 'callacagada':
             message.reply('Vash ctmre');
+          break;
+          case 'version':
+            message.reply('Crrabot version v0.1. Hay talento, falto apoyo!' + '\n' + 'By Cuzz, the creator');
           break;
           case 'test':
             const embed = new MessageEmbed()
@@ -49,11 +83,10 @@ if(messageCommand.startsWith(PREFIX))
             message.channel.send(attachment);
           break;
         default:
-            message.reply('Auxilio dame contexto');
+            message.reply('Auxilio dame contexto, pon un comando correcto pss kagada!');
           break;
       }
 
-   
     
 }
 });
